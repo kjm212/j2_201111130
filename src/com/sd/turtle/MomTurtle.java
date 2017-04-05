072ec0fe7e7512ae2671775a693f97ca1b2f60f3
김종목 <kimjm212@naver.com>
2017-04-05 20:57:59 +0900

    Add files via upload

---
+++ src/com/sd/turtle/MomTurtle.java
@@ -0,0 +1,17 @@
+package com.sd.turtle;
+
+import java.util.Observer;
+import java.util.Observable;
+import java.util.ArrayList;
+import java.awt.geom.Point2D.Double;
+import java.awt.Color;
+import ch.aplu.turtle.*;
+
+public class MomTurtle extends Turtle implements Observer{
+  Double babyCurpos;
+  
+  public void update(Observable o, Object arg){
+    babyCurpos = ((BabyTurtle)o).myCurpos;
+    moveTo(babyCurpos);
+  }
+}
---
+++ src/com/sd/turtle/TurtleObserverMain.java
@@ -0,0 +1,21 @@
+package com.sd.turtle;
+
+import java.util.Observer;
+import java.util.Observable;
+import java.util.ArrayList;
+import java.awt.geom.Point2D.Double;
+import java.awt.Color;
+import ch.aplu.turtle.*;
+
+public class TurtleObserverMain {
+    public static void main(String[] args) {
+        System.out.println("--Observer pattern demo");
+        BabyTurtle baby=new BabyTurtle();
+        MomTurtle mom=new MomTurtle();
+        baby.addObserver(mom);
+        mom.moveTo(100,100);
+        baby.move(100,-100);
+        baby.setChanged();
+        baby.notifyObservers();
+    }
+}
